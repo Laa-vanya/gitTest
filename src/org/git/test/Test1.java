34cebdfe5bcbe444500009419c22a210dddd167f
Laa-vanya <lava3jeevi@gmail.com>
2021-06-09 17:06:10 +0530

    testing git

---
+++ src/org/git/test/Test1.java
@@ -0,0 +1,15 @@
+package org.git.test;
+
+public class Test1 {
+	int a=10;
+	int b=20;
+	public void add() {
+		int c=a+b;
+		System.out.println(c);
+	}
+	
+public static void main(String[] args) {
+	Test1 t=new Test1();
+	t.add();
+}
+}
