e0a226387942ba3fbfc0d65f8335c335e3478093
Laa-vanya <lava3jeevi@gmail.com>
2021-06-10 14:11:04 +0530

    updated one

--- src/org/git/test/Test1.java
+++ src/org/git/test/Test1.java
@@ -1,15 +1,11 @@
 package org.git.test;
 
 public class Test1 {
-	int a=10;
-	int b=20;
-	public void add() {
-		int c=a+b;
-		System.out.println(c);
+	public static void main(String[] args) {
+		System.out.println("lava");
+	}
+		
 	}
 	
-public static void main(String[] args) {
-	Test1 t=new Test1();
-	t.add();
-}
-}
+
+
